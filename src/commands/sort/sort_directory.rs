a7dee5c8a0191662658d21aa88ff0f0f51c023d8
Matej Petuchovsky <petuchovsky@gmail.com>
2026-02-15 15:14:34 +0100

    sort directory unit test

--- src/commands/sort/sort_directory.rs
+++ src/commands/sort/sort_directory.rs
@@ -38,3 +38,31 @@ fn collect_directories(files_list: FilesList) -> anyhow::Result<(Vec<DirEntry>, 
 
     Ok((files, inner_directories_paths))
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{commands::sort::FilesList, volumes::get_path_under_tests};
+
+    #[test]
+    fn test_collect_directories() {
+        let file_list =
+            FilesList::new(&get_path_under_tests("sort_directory"), false, false).unwrap();
+        let (files, inner_directories) = super::collect_directories(file_list).unwrap();
+        let files_names: Vec<String> = files
+            .into_iter()
+            .map(|entry| entry.file_name().to_string_lossy().to_string())
+            .collect();
+        let inner_directories_names: Vec<String> = inner_directories
+            .into_iter()
+            .map(|path| path.file_name().unwrap().to_string_lossy().to_string())
+            .collect();
+        println!("Files: {files_names:?}");
+        println!("Inner directories: {inner_directories_names:?}");
+        assert_eq!(files_names.len(), 2);
+        assert_eq!(inner_directories_names.len(), 2);
+        assert!(files_names.contains(&"file1.txt".to_string()));
+        assert!(files_names.contains(&"file2.txt".to_string()));
+        assert!(inner_directories_names.contains(&"inner_dir_1".to_string()));
+        assert!(inner_directories_names.contains(&"inner_dir_2".to_string()));
+    }
+}
